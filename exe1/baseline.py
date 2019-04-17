d3e28c3515b2209ac118d3fd7dfc9d5eefc9fa1d
Forsworns <378974295@qq.com>
2019-04-17 21:07:38 +0800

    result

--- exe1/baseline.py
+++ exe1/baseline.py
@@ -40,36 +40,6 @@ def SVM_recommend_run(model_name, X_train, X_test, y_train, y_test, paras={}, **
             model_name, paras, sc, f1_sc))
         return clf
 
-def SVM_compare_run(model_name, X_train, X_test, y_train, y_test, paras={}, **SVM_paras):
-    # 统一用于后续实验比较的SVM，提供前六个参数
-    if SVM_paras == {}:
-        SVM_paras = {'C': 2, 'kernel': 'rbf', 'decision-function-shape': 'ovo'}
-    if paras == {}:
-        paras.update(SVM_paras)
-    result = load_result(model_name, paras)
-    if result is None:
-        clf = load_model(model_name, paras) # 有问题，训练集和测试集变了
-        if clf is None:
-            print("can't find clf",model_name)
-            clf = SVC(**SVM_paras)
-            clf.fit(X_train, y_train)
-            save_model(clf, model_name, paras)
-        y_pred = clf.predict(X_test)
-        sc = clf.score(X_test, y_test)
-        # unweighted mean of metrics for labels
-        f1_sc = f1_score(y_test, y_pred, average='macro')
-        result = {'score': sc, 'f1_score': f1_sc}
-        save_result(result, model_name, paras)
-        print("{} with {}: score is {}, f1_score is {}".format(
-            model_name, paras, sc, f1_sc))
-        return clf
-    else:
-        clf = load_model(model_name, paras)
-        sc, f1_sc = result.values()
-        print("{} with {}: score is {}, f1_score is {}".format(
-            model_name, paras, sc, f1_sc))
-        return clf
-
 
 def SVM_base(X, y):
     # model_name为采用的降维方法，X为降维后的feature数据
@@ -77,7 +47,7 @@ def SVM_base(X, y):
     for d in DECI_FUNCS:
         for k in KERNELS:
             for C in CS:
-                SVM_compare_run(COMPARE, X_train, X_test, y_train, y_test, paras={}, C=C, kernel=k, decision_function_shape=d)
+                SVM_recommend_run(COMPARE, X_train, X_test, y_train, y_test, paras={}, C=C, kernel=k, decision_function_shape=d)
             '''
             for C in CS:
                 SVM_recommend_run(BASELINE, X_train, X_test, y_train, y_test, paras={}, C=C)
---
+++ exe3/lib/RAEDME.md
@@ -0,0 +1,7 @@
+```matlab
+run('vlfeat-0.9.21/toolbox/vl_setup') 
+vl_version verbose 
+```
+
+
+
